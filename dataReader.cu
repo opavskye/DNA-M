d6965323755cf3c6f436ed9a92c610e19893f0b2
Erik Opavsky <opavskye@cs.grinnell.edu>
2012-07-26 23:47:44 -0500

    intermediate save

--- dataReader.cu
+++ dataReader.cu
@@ -32,26 +32,38 @@ int main (int argc, char *argv[]) {
   
   char fileName[100];
   strcpy (fileName, "../data/");
+
   int numSequences = 125;
   int sequenceLength = 201;
-  char ** sequences = (char **) malloc (numSequences * sizeof (char *));
+
+  // allocate memory for sequences
+  char ** sequences =  sequences = (char **) malloc (numSequences * sizeof (char *));
   for (int i = 0; i < numSequences; i++)
     *(sequences + i) = (char *) malloc (sequenceLength * sizeof (char));
 
-  // append argv[1] to the end of path to data folder
-  int i = 0;
-  if (argc > 1) 
-    while ((fileName[(i++) + 8] = argv[1][i]) != '\0');
-  else {
-    printf ("Please run again with a filename input from the data folder.\n");
-    return 1;
-  }
+
+  if (argc >= 1) 
+    { 
+      // append argv[1] to the end of path to data folder
+      int i = 0; 
+      while ((fileName[(i++) + 8] = argv[1][i]) != '\0');
+    } 
+  else
+    {
+      printf ("Please run again with a filename input from the data folder.\n");
+      return 1;
+    }
 
   // read in the data
   if (!readSequences (fileName, sequences, numSequences, sequenceLength))
     printf ("error reading data\n");
 
-  // printSequences (sequences, numSequences);
+  printSequences (sequences, numSequences);
+
+  // free all allocated memory
+  for (int i = 0; i < numSequences; i++)
+    free (sequences[i]);
+  free (sequences);
 
   return 0;
 }
