86eca98f12317581c2ec1ef55ac77543b2272b9d
Erik <erik@erik-N73JQ.(none)>
2012-08-17 10:39:45 -0500

    extractor working, possibly final version

--- extractor.c
+++ extractor.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <float.h>
+
+int isSpaceOrTab (char c) {
+  return (c == ' ' || c == '\t');
+}
+
+int isWhiteOrQuote (char c) {
+  return (isspace (c) || c == '"');
+}
 
 int analyzeHeader (char * outFileName, char * inFileName, char * columnString) {
   FILE * inFile;
@@ -54,6 +64,36 @@ int analyzeHeader (char * outFileName, char * inFileName, char * columnString) {
   return distance;
 }
 
+double getFromColumn (char * source, int columnNum) {
+  
+  char c;
+  int i, j = 0;
+
+  // to store the value in the query column
+  char val[50];
+
+  for (i = 0; i < columnNum; i++) {
+
+    // go through ith column
+    while (!isSpaceOrTab (c = source[j++]))
+      if (c == '\n')
+        return DBL_MAX;
+
+    // go through ith whitespace
+    while (isSpaceOrTab (c = source[j++]))
+      if (c == '\n')
+        return DBL_MAX;
+  }
+
+  i = 0;
+  while (!isWhiteOrQuote (c = source[j]))
+    val[i++] = source[j++];
+
+  val[i] = '\0';
+
+  return atof (val);
+}
+
 int main (int argc, char *argv[]) {
 
   if (argc < 5) {
@@ -61,8 +101,8 @@ int main (int argc, char *argv[]) {
     return 1;
   }
 
-  int minBound = atoi (argv[1]);
-  int maxBound = atoi (argv[2]);
+  double minBound = atof (argv[1]);
+  double maxBound = atof (argv[2]);
   char * outFileName = argv[3];
   FILE * outFile;
   int numFiles = argc - 4;
@@ -71,11 +111,21 @@ int main (int argc, char *argv[]) {
   char temp;
   char * columnString = "\"Distance_TSS\"";
 
+  int maxRowLength = 20000;
+  char row[maxRowLength];
+  double value;
+
   // find which column the columnString is in and transfer the header from inFile[0] to outFile
   int distanceColumn = analyzeHeader(outFileName, inFile[0], columnString);
 
   if (distanceColumn == -1)
     return 1;
+
+  // open output file
+  if (!(outFile = fopen (outFileName, "a"))) {
+    printf ("Error opening file %s.  Aborting.\n", outFileName);
+    return 1;
+  }
 
   // go through all files
   int i;
@@ -90,11 +140,24 @@ int main (int argc, char *argv[]) {
     // skip first header row of the current file
     while ((temp = fgetc (currentFile)) != '\n');
 
-    
-    
+    // go through all rows, checking them one at a time
+    while (fgets (row, maxRowLength, currentFile) != NULL) {
+      
+      // get query column value
+      value = getFromColumn (row, distanceColumn);
+
+      // check bounds and copy row over if value fits
+      if (value >= minBound && value <= maxBound)
+        fprintf (outFile, "%s", row);
+
+    }
+
     fclose (currentFile);
   }
 
+  fclose (outFile);
+
+  // check if we've gone through all of the files
   if (i == numFiles)
     printf ("All %d files have been successfully processed.\n", numFiles);
   else
